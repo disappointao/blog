4d35e3bcdab5834016f67794f0b390053917a06e
ao <1147608791@qq.com>
2019-09-06 17:16:56 +0800

    入口

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express=require('express');
 const path=require('path');
-const router=require('./router');
+const user=require('./router/user');//user登录路由
+const bodyParser=require('body-parser');
+const session=require('express-session');
 let app=express();
 //设置静态资源请求
 app.use('/public/',express.static(path.join(__dirname,'./public/')));
@@ -8,8 +10,16 @@ app.use('/public/',express.static(path.join(__dirname,'./public/')));
 app.engine('html',require('express-art-template'));
 //配置默认模板渲染路径
 app.set('views',path.join(__dirname,'./views'));
-//把路由挂在到app中
-app.use(router);
+
+//配置bodyParser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+  secret:'itcast',
+  resave:false,
+  saveUninitialized:true
+}));
+app.use(user);
 app.listen(3000,function() {
   console.log('服务器启动...');
 });
